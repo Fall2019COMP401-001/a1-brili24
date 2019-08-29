422de4eea679f193733bc414d24259b3eee329a2
Brianna Li <brili24@live.unc.edu>
2019-08-29 00:14:16 -0400

    attempt 2

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -9,22 +9,46 @@ public class A1Novice {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+		
+		// declare total number of customers
 		int totalCustomers = scan.nextInt();
 		
+		// initial loop for total customers
 		for (int i=0; i<=totalCustomers; i++) {
+			
+			// store first name of customer
 			String firstName = scan.next();
+			
+			// get first initial
+			char firstInitial = firstName.charAt(0);
+			
+			// store last name of customer
 			String lastName = scan.next();
+			
+			// declare number of items bought by customer
 			int numberOfItems = scan.nextInt();
 			
+			// declare number of each item bought
 			int numberOfEachItem = scan.nextInt();
-			String itemName = scan.next();
-			double total = scan.nextDouble();
 			
+			double total = 0;
+			
+			// second loop for number of items
 			for (int j=0; j<=numberOfItems; j++) {
-				total = total + scan.nextDouble();
+				
+				// declare item name, not really used
+				String itemName = scan.next();
+				
+				// declare price of item
+				double price = scan.nextDouble();
+				
+				// add and multiply price to the total cost
+				total = total + price * numberOfEachItem;
 			}
-		scan.close();
-		System.out.println(firstName + lastName + ": " + total);
+			
+		// print output	
+		System.out.println(firstInitial + ". " + lastName + ": " + total);
+		
 		}
 	}
 }
