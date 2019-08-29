f6152be2c1c3d27452edd7e475d25522324321a9
Brianna Li <brili24@live.unc.edu>
2019-08-28 23:29:47 -0400

    new branch

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -9,6 +9,22 @@ public class A1Novice {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+		int totalCustomers = scan.nextInt();
 		
+		for (int i=0; i<=totalCustomers; i++) {
+			String firstName = scan.next();
+			String lastName = scan.next();
+			int numberOfItems = scan.nextInt();
+			
+			int numberOfEachItem = scan.nextInt();
+			String itemName = scan.next();
+			double total = scan.nextDouble();
+			
+			for (int j=0; j<=numberOfItems; j++) {
+				total = total + scan.nextDouble();
+			}
+		scan.close();
+		System.out.println(firstName + lastName + ": " + total);
+		}
 	}
 }
